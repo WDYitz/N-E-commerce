3fc2eb7f5bc846a19aedf79951fc08b138f802b9
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-08-28 19:32:00 -0300

    feat: Add ScreensInDevelopment component to improve user experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "@/styles/globals.css";
 import { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { PropsWithChildren } from "react";
+import ScreensInDevelopment from "./screen-development";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -13,14 +14,14 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "None E-commerce",
   description: "Melhores produtos eletrônicos",
-}
+};
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <html lang="pt-BR" className="dark">
       <body className={poppins.className}>
         <Header />
-        {children}
+        <ScreensInDevelopment>{children}</ScreensInDevelopment>
         <Footer />
       </body>
     </html>
