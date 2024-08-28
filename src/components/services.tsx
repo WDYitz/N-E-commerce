87301db5d077d10ee27112bb0a2a3c68440b42ed
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-08-28 16:15:42 -0300

    feat: Add unique constraint for categories table

---
+++ src/components/services.tsx
@@ -0,0 +1,24 @@
+import { ServicesDataType } from "@/data/services-data";
+import { Card, CardContent } from "./ui/card";
+
+interface AdvantagesProps {
+  service: ServicesDataType;
+}
+
+const Services = ({ service }: AdvantagesProps) => {
+  return (
+    <Card className="p-2 h-[230px] w-[46%] hover:border-indigo-400 transition-all">
+      <CardContent className="p-0 flex flex-col justify-center items-center gap-3 h-full">
+        {service.icon}
+        <h3 className="text-md font-semibold text-neutral-300">
+          {service.title}
+        </h3>
+        <p className="text-sm text-neutral-400 text-center">
+          {service.description}
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default Services;
