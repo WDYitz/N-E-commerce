ebc41d3fd72323bd81a1f6000cdc1e0d398d8722
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-08-28 19:32:44 -0300

    refactor: Update Sidebar component to use ShoppingCart icon instead of MenuIcon

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,17 +1,13 @@
 "use client";
-import { Links } from "@/data/links";
-import { MenuIcon } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 import { usePathname } from "next/navigation";
-import Category from "./category";
 import { Button } from "./ui/button";
 import {
   Sheet,
-  SheetClose,
   SheetContent,
   SheetFooter,
   SheetHeader,
-  SheetTitle,
-  SheetTrigger,
+  SheetTrigger
 } from "./ui/sheet";
 
 const Sidebar = () => {
@@ -20,28 +16,13 @@ const Sidebar = () => {
     <Sheet>
       <SheetTrigger asChild>
         {/* Menu Button */}
-        <Button variant="secondary" className="text-white">
-          <MenuIcon size={20} className="text-primary" />
+        <Button variant="ghost" className="text-white">
+          <ShoppingCart size={24} className="text-primary" />
         </Button>
       </SheetTrigger>
       <SheetContent>
-        <SheetHeader>
-          <SheetTitle className="text-left">Menu</SheetTitle>
-        </SheetHeader>
+        <SheetHeader></SheetHeader>
         {/* Links */}
-        <SheetClose asChild>
-          <div className="mt-5 space-y-2">
-            {Links.map((link, index) => (
-              <Category
-                key={index}
-                category={link}
-                className={
-                  path === "/category" + link.url ? "bg-secondary" : ""
-                }
-              />
-            ))}
-          </div>
-        </SheetClose>
         <SheetFooter></SheetFooter>
       </SheetContent>
     </Sheet>
