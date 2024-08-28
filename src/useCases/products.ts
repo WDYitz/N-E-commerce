e8787d281cf0530415cac9d6e6d34f0933ab0ad1
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-08-28 01:23:37 -0300

    refactor: Update ProductUseCase constructor to accept productRepositoryData

--- src/useCases/products.ts
+++ src/useCases/products.ts
@@ -2,11 +2,7 @@ import { ProductsData, ProductType } from "@/data/products-data";
 import { UUID } from "crypto";
 
 class ProductUseCase {
-  private productRepositoryData: ProductType[];
-
-  constructor() {
-    this.productRepositoryData = ProductsData;
-  }
+  constructor(private productRepositoryData: ProductType[]) { }
 
   getProductById = (id: UUID): ProductType => {
     return this.productRepositoryData.filter((prod) => prod.id === id)[0];
@@ -19,4 +15,4 @@ class ProductUseCase {
 
 // The '[0]' at the end of the 'getProductById' method is to return the first element of the array, since the filter method returns an array of elements that match the condition. In this case, we are assuming that the id and category are unique, so we can safely return the first element of the array.
 
-export const productUseCase = new ProductUseCase();
+export const productUseCase = new ProductUseCase(ProductsData);
