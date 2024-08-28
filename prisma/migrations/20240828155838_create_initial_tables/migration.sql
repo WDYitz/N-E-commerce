4d6c4abdc472cc2b82ad34b5860d832ebc18490f
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-08-28 13:50:20 -0300

    refactor: Update .gitignore and package.json for Prisma migration lock file and dependencies

---
+++ prisma/migrations/20240828155838_create_initial_tables/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "price" DECIMAL(10,2) NOT NULL,
+    "discount_percentage" INTEGER NOT NULL DEFAULT 0,
+    "quantity" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "stars" INTEGER NOT NULL,
+    "categoryId" TEXT NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
