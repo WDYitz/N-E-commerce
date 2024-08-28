42bcdc51f3804694e3cf9b2f6df7a99143e757b4
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-08-28 19:32:15 -0300

    feat: Add react-use library for enhanced functionality

--- src/app/category/[name]/[id]/page.tsx
+++ src/app/category/[name]/[id]/page.tsx
@@ -3,6 +3,7 @@ import FavoriteButton from "@/components/favoriteButton";
 import Stars from "@/components/stars";
 import { Separator } from "@/components/ui/separator";
 import { productUseCase } from "@/useCases/products";
+import Image from "next/image";
 
 import { notFound } from "next/navigation";
 
@@ -12,26 +13,30 @@ interface ProductPageProps {
   };
 }
 const ProductPage = async ({ params }: ProductPageProps) => {
-  const products = await productUseCase.getProductById(params.id);
+  const { category, product } =
+    await productUseCase.getProductsByIdAndCategories(params.id);
 
-  if (!products) {
+  if (!category || !product) {
     notFound();
   }
 
   return (
     <main className="flex flex-col p-5 space-y-4">
       <div className="pt-2 pb-2">
-        <BreadcrumbNavigator product={products} category={undefined} />
+        <BreadcrumbNavigator product={product} category={category} />
       </div>
       <Separator />
       <div className="flex flex-col">
         <div className="flex justify-between">
-          <h2>{products?.name}</h2>
+          <h2>{product.name}</h2>
           <FavoriteButton />
         </div>
         <div className="flex">
-          <Stars rating={products?.stars ?? 0} />
+          <Stars rating={product.stars ?? 0} />
         </div>
+      </div>
+      <div className="py-2 relative w-full max-w-[300px] min-h-[250px] flex justify-center items-center">
+        <Image alt={product.name} src={product.imageURL} fill />
       </div>
     </main>
   );
--- src/app/category/[name]/page.tsx
+++ src/app/category/[name]/page.tsx
@@ -2,7 +2,6 @@ import BreadcrumbNavigator from "@/components/breadCrumbNavigator";
 import ProductCard from "@/components/product-card";
 import { categoryUseCase } from "@/useCases/category";
 import { productUseCase } from "@/useCases/products";
-import { Suspense } from "react";
 
 interface CategoryPageProps {
   params: {
@@ -14,7 +13,9 @@ const CategoryPage = async ({ params }: CategoryPageProps) => {
   const category = await categoryUseCase.getCategoryByName(params.name);
 
   if (!category) {
-    return <div className="px-5 text-md text-slate-400">Category not found</div>;
+    return (
+      <div className="px-5 text-md text-slate-400">Category not found</div>
+    );
   }
 
   const products = await productUseCase.getProductsByCategoryId(category.id);
@@ -26,15 +27,9 @@ const CategoryPage = async ({ params }: CategoryPageProps) => {
       </div>
       <h2>{params.name.toLocaleUpperCase()}</h2>
       <div className="grid grid-cols-1 gap-4">
-        <Suspense fallback={<div>...Loading</div>}>
-          {products.map((product) => (
-            <ProductCard
-              product={product}
-              key={product.id}
-              category={category}
-            />
-          ))}
-        </Suspense>
+        {products.map((product) => (
+          <ProductCard product={product} key={product.id} category={category} />
+        ))}
       </div>
     </main>
   );
