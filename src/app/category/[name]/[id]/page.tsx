43b99c32fbe06afe569bda4bbf78b073c9630783
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-09-03 19:45:23 -0300

    refactor: Update page.tsx to use useProductsFactory hook for fetching product data

--- src/app/category/[name]/[id]/page.tsx
+++ src/app/category/[name]/[id]/page.tsx
@@ -1,8 +1,11 @@
 import BreadcrumbNavigator from "@/components/breadCrumbNavigator";
 import FavoriteButton from "@/components/favoriteButton";
+import ProductCard from "@/components/product-card";
 import Stars from "@/components/stars";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import { productUseCase } from "@/useCases/products";
+import useProductsFactory from "@/hooks/use-products-factory";
 import { services } from "@/useCases/services";
 import Image from "next/image";
 import { notFound } from "next/navigation";
@@ -13,12 +16,15 @@ interface ProductPageProps {
   };
 }
 const ProductPage = async ({ params }: ProductPageProps) => {
-  const { category, product } =
-    await productUseCase.getProductsByIdAndCategories(params.id);
+  const { category, product, relatedProducts } = await useProductsFactory(
+    params.id
+  );
 
   if (!category || !product) {
     notFound();
   }
+
+  const hasDiscount = product.discountPercentage > 0;
 
   return (
     <main className="flex flex-col p-5 space-y-4">
@@ -29,7 +35,12 @@ const ProductPage = async ({ params }: ProductPageProps) => {
       <div className="flex flex-col">
         <div className="flex justify-between">
           <h2>{product.name}</h2>
-          <FavoriteButton />
+          <div className="flex gap-4 items-center">
+            <span className="text-xs text-slate-300">
+              qty: {product.quantity}
+            </span>
+            <FavoriteButton />
+          </div>
         </div>
         <div className="flex">
           <Stars rating={product.stars ?? 0} />
@@ -40,9 +51,16 @@ const ProductPage = async ({ params }: ProductPageProps) => {
           <Image alt={product.name} src={product.imageURL} fill />
         </div>
       </div>
-      <p className="text-xs text-slate-400 font-semibold">
-        {product.quantity < 1 ? "Fora de Estoque" : "Em Estoque"}
-      </p>
+      <div className="flex justify-between items-center">
+        <p className="text-xs  text-slate-200 font-semibold">
+          {product.quantity < 1 ? "Fora de Estoque" : "Em Estoque"}
+        </p>
+        {hasDiscount && (
+          <Badge variant="default" className="w-20 flex justify-center">
+            {product.discountPercentage}% OFF
+          </Badge>
+        )}
+      </div>
       <div className="flex flex-col space-y-2">
         <span className="text-sm line-through">
           {services.formatCurrency(Number(product.price))}
@@ -56,9 +74,30 @@ const ProductPage = async ({ params }: ProductPageProps) => {
           )}
         </span>
         <p className="font-light text-sm">
-          Com descontos de{" "}
-          <span className="font-bold">{product.discountPercentage}%</span>
+          Com descontos de
+          <span className="font-bold"> {product.discountPercentage}%</span>
         </p>
+        <Button>{product.quantity < 1 ? "Avise-me" : "Comprar"}</Button>
+      </div>
+      <div className="flex flex-col space-y-2 pt-6">
+        <span className="uppercase font-semibold">produtos relacionados</span>
+        <div className="flex overflow-x-scroll [&::-webkit-scrollbar]:hidden gap-4">
+          {relatedProducts.map(
+            (product) =>
+              product.id !== params.id && (
+                <ProductCard
+                  product={product}
+                  key={product.id}
+                  category={category}
+                />
+              )
+          )}
+        </div>
+      </div>
+      <div className="flex flex-col pt-6 space-y-2">
+        <p className="uppercase font-semibold">descrição do produto</p>
+        <p className="text-xs uppercase">{product.name}</p>
+        <p className="text-xs">{product.description}</p>
       </div>
     </main>
   );
---
+++ src/hooks/use-products-factory.tsx
@@ -0,0 +1,14 @@
+import { productUseCase } from "@/useCases/products";
+
+const useProductsFactory = async (id: string) => {
+  const { category, product } =
+    await productUseCase.getProductsByIdAndCategories(id);
+
+  const relatedProducts = await productUseCase.getProductsByCategoryId(
+    category.id
+  );
+
+  return { category, product, relatedProducts };
+};
+
+export default useProductsFactory;
