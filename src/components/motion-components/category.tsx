bcf57a421d72e236f693eae4c6f9b9e7bd68369f
Yitzhak Rodriguez <yitzhakrodriguez.0901@gmail.com>
2024-11-30 16:02:30 -0300

    Refactor: Add motion components for cart item, add-to-cart button, and category links

---
+++ src/components/motion-components/category.tsx
@@ -0,0 +1,40 @@
+import { cn } from "@/lib/utils";
+import { Category } from "@prisma/client";
+import { motion } from "motion/react";
+import Link from "next/link";
+import { forwardRef } from "react";
+import { Button } from "../ui/button";
+
+interface CategoryProps {
+  category: Category;
+  className?: string;
+  icon?: React.ReactNode;
+}
+
+const CategoryLinks = forwardRef<HTMLButtonElement, CategoryProps>(({ category, className, icon }, ref) => {
+  // MEMORY PERFORMANCE: Avoid using string concatenation in the render method
+  const URI_CATEGORY_FULL_PREFIX = "/category/" + category.name;
+  const URI_CATEGORY_SINGLE_PREFIX = "/" + category.name;
+  const URI_HOME_PREFIX = "/";
+  const url =
+    URI_CATEGORY_SINGLE_PREFIX !== URI_HOME_PREFIX
+      ? URI_CATEGORY_FULL_PREFIX
+      : URI_HOME_PREFIX;
+
+  return (
+    <Button variant="ghost" asChild ref={ref} className="text-left w-full">
+      <Link
+        href={url}
+        className={cn("hover:bg-transparent", className)}
+      >
+        {icon}
+        {category.name.charAt(0).toUpperCase() + category.name.slice(1)}
+      </Link>
+    </Button>
+  );
+})
+
+CategoryLinks.displayName = "CategoriesLink";
+const CategoryLinksMotion = motion(CategoryLinks);
+
+export default CategoryLinksMotion;
